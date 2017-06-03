4dd8c36b6e6826534d589ba607e43f113d354bb2
Chris Armstrong <chris.r.armstrong@gmail.com>
2017-06-03 10:54:27 -0400

    Add navigation bar with links

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   resources :arguments
   resources :sources
   resources :premises
-  get 'home/index'
 
-  root 'home#index'
+  root 'arguments#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
