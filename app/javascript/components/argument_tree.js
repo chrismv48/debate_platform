50f8988d7d569f50c5ce099fd3713e2268612352
Chris Armstrong <chris.r.armstrong@gmail.com>
2017-06-04 14:31:16 -0400

    Remove jsplumb in favor of d3. Add d3 tree example.

--- app/javascript/components/argument_tree.js
+++ app/javascript/components/argument_tree.js
@@ -1,8 +1,15 @@
 import React, {Component} from 'react'
-import {ajax} from 'jquery';
-import Select from 'react-select';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
+import {stratify, tree} from 'd3-hierarchy'
+import * as d3 from "d3";
 
+let testData = [
+  {id: 1, parentId: null},
+  {id: 2, parentId: 1},
+  {id: 3, parentId: 1},
+  {id: 4, parentId: 2},
+  {id: 5, parentId: 2},
+]
 
 export default class ArgumentTree extends Component {
 
@@ -14,11 +21,63 @@ export default class ArgumentTree extends Component {
     };
   }
 
+  componentDidMount() {
+    this.drawTree()
+  }
+
+  drawTree() {
+    var svg = d3.select("svg"),
+      width = +svg.attr("width"),
+      height = +svg.attr("height"),
+      g = svg.append("g").attr("transform", "translate(40,0)");
+
+    var tree = d3.tree()
+      .size([height, width - 160]);
+
+    var root = stratify()(testData)
+
+    var link = g.selectAll(".link")
+      .data(tree(root).links())
+      .enter().append("path")
+      .attr("class", "link")
+      .attr("d", d3.linkHorizontal()
+        .x(function (d) {
+          return d.y;
+        })
+        .y(function (d) {
+          return d.x;
+        }));
+
+    var node = g.selectAll(".node")
+      .data(root.descendants())
+      .enter().append("g")
+      .attr("class", function (d) {
+        return "node" + (d.children ? " node--internal" : " node--leaf");
+      })
+      .attr("transform", function (d) {
+        return "translate(" + d.y + "," + d.x + ")";
+      })
+
+    node.append("circle")
+      .attr("r", 2.5);
+
+    node.append("text")
+      .attr("dy", 3)
+      .attr("x", function (d) {
+        return d.children ? -8 : 8;
+      })
+      .style("text-anchor", function (d) {
+        return d.children ? "end" : "start";
+      })
+      .text(function (d) {
+        return d.id.substring(d.id.lastIndexOf(".") + 1);
+      });
+
+  }
+
   render() {
     return (
-      <div>
-        Hello world!
-      </div>
+      <svg width="960" height="2000"></svg>
     )
   }
 }
