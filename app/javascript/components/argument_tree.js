36de54ee7e660d6196e3920ab23adf2a947a5d53
Chris Armstrong <chris.r.armstrong@gmail.com>
2017-07-01 17:33:02 -0400

    Clean up argument tree component

--- app/javascript/components/argument_tree.js
+++ app/javascript/components/argument_tree.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+
+import EditNewPremise from '../components/edit_new_premise'
+
 import {stratify, tree} from 'd3-hierarchy'
 import * as d3 from "d3";
 import _ from 'underscore'
 import {Modal} from 'react-bootstrap'
-import EditNewPremise from '../components/edit_new_premise'
 
+const NODE_HEIGHT = 50
+const NODE_WIDTH = 180
+const TREE_LAYOUT_WIDTH = 850
+const TREE_LAYOUT_HEIGHT = 450
+const SVG_LAYOUT_WIDTH = 900
+const SVG_LAYOUT_HEIGHT = 500
+const ROOT_NODE_MARGIN = 10
 
 export default class ArgumentTree extends Component {
 
@@ -36,14 +45,17 @@ export default class ArgumentTree extends Component {
   }
 
   toggleSubTreeVisibility(nodeId) {
-    if (_.has(this.state.hiddenPremises, nodeId)) {
-      const newHiddenPremises = _.omit(this.state.hiddenPremises, nodeId)
+    const {hiddenPremises} = this.state
+
+    if (_.has(hiddenPremises, nodeId)) {
+      const newHiddenPremises = _.omit(hiddenPremises, nodeId)
+      console.log(newHiddenPremises)
       this.setState({hiddenPremises: newHiddenPremises})
     }
     else {
       const subTreeIds = this.getSubTree(nodeId)
       if (subTreeIds) {
-        let newHiddenPremises = {...this.state.hiddenPremises}
+        let newHiddenPremises = {...hiddenPremises}
         newHiddenPremises[nodeId] = subTreeIds
         this.setState({hiddenPremises: newHiddenPremises})
       }
@@ -55,34 +67,62 @@ export default class ArgumentTree extends Component {
     this.setState({selectedPremise: _.find(this.state.tree, (node) => node.premise.id === premiseId).premise})
   }
 
-  render() {
-    const nodeHeight = 50
-    const nodeWidth = 180
-    const hiddenPremises = _.flatten(_.values(this.state.hiddenPremises))
-    const root = d3.stratify()
+  generateRoot() {
+    let {tree} = this.state
+
+    return d3.stratify()
       .id((d) => d.premise.id)
       .parentId(d => d.connection ? d.connection.parent_premise_id : null)
-      (this.state.tree);
+      (tree);
+  }
 
-    const treeLayout = d3.tree();
-    treeLayout.size([950,450])
-    const tree = treeLayout(root)
-    let nodesList = tree.descendants();
-    nodesList[0].y += 10
-    let linksList = tree.links()
-    const lineLink = d3.linkVertical()
-      .x(d => d[0])
-      .y(d => d[1]);
-
-    const divNodes = nodesList.map(node => {
+  generateTreeNodes(nodes) {
+    return nodes.map(node => {
+      const premiseId = node.data.premise.id
+      const premiseName = node.data.premise.name
+      const {premiseHovered} = this.state
       return (
-        <foreignObject x={node.x} y={node.y} width={nodeWidth} height={nodeHeight}>
+        <g key={premiseId} className="node"
+           transform={`translate(${node.x}, ${node.y})`}
+           fill={premiseHovered === premiseId ? "yellow" : "grey"}
+           onMouseEnter={(event) => this.setState({premiseHovered: premiseId})}
+           onMouseLeave={() => this.setState({premiseHovered: null})}
+           onClick={() => this.toggleSubTreeVisibility(premiseId)}
+           visibility={this.getHiddenPremises().includes(premiseId) ? "hidden" : "visible"}
+        >
+          <rect id={premiseId}
+                width={NODE_WIDTH}
+                height={NODE_HEIGHT}
+                strokeWidth="1"
+                fillOpacity={0.1}
+                stroke="black"
+          />
+          <text
+            x={NODE_WIDTH / 2}
+            y={NODE_HEIGHT / 2}
+            textAnchor="middle"
+            fill="black">{premiseName}
+          </text>
+          <foreignObject x={NODE_WIDTH - 50} y={NODE_HEIGHT - 20} width="45" height="20">
+            <span onClick={() => this.handleModifyPremise(premiseId)}
+                  style={{position: "static"}}
+                  className="glyphicon glyphicon-plus"/>
+            <span style={{position: "static"}} className="glyphicon glyphicon-pencil"/>
+            <span style={{position: "static"}} className="glyphicon glyphicon-trash"/>
+          </foreignObject>
+        </g>
+      );
+    });
+
+  }
+
+  generateDivNodes(nodes) {
+    return nodes.map(node => {
+      return (
+        <foreignObject x={node.x} y={node.y} width={NODE_WIDTH} height={NODE_HEIGHT}>
           <div style={{
-            // left: node.x,
-            // top: node.y,
-            // position: "relative",
-            width: nodeWidth,
-            height: nodeHeight,
+            width: NODE_WIDTH,
+            height: NODE_HEIGHT,
             borderStyle: "solid",
             display: "inline-block"
           }}
@@ -92,62 +132,65 @@ export default class ArgumentTree extends Component {
         </foreignObject>
       )
     })
-    /* render the nodes */
-    const nodes = nodesList.map(node => {
-      return (
-        <g key={node.data.premise.id} className="node"
-           transform={`translate(${node.x}, ${node.y})`}
-           fill={this.state.premiseHovered === node.data.premise.id ? "yellow" : "grey"}
-           onMouseEnter={(event) => this.setState({premiseHovered: node.data.premise.id})}
-           onMouseLeave={() => this.setState({premiseHovered: null})}
-           onClick={() => this.toggleSubTreeVisibility(node.data.premise.id)}
-           visibility={hiddenPremises.includes(node.data.premise.id) ? "hidden" : "visible"}
-        >
-          <rect id={node.data.premise.id}
-                width={nodeWidth}
-                height={nodeHeight}
-                strokeWidth="1"
-                fillOpacity={0.1}
-                stroke="black"
-          />
-          <text x={nodeWidth / 2} y={nodeHeight / 2} textAnchor="middle" fill="black">{node.data.premise.name}</text>
-          <foreignObject x={nodeWidth - 50} y={nodeHeight - 20} width="45" height="20">
-            <span onClick={() => this.handleModifyPremise(node.data.premise.id)}
-                  style={{position: "static"}}
-                  className="glyphicon glyphicon-plus"/>
-            <span style={{position: "static"}} className="glyphicon glyphicon-pencil"></span>
-            <span style={{position: "static"}} className="glyphicon glyphicon-trash"></span>
-          </foreignObject>
-        </g>
-      );
-    });
+  }
 
-    /* render the links */
-    const links = linksList.map((link, i) => {
+  getHiddenPremises() {
+    return _.flatten(_.values(this.state.hiddenPremises))
+  }
+
+  generateLinkPaths(links) {
+    const lineLink = d3.linkVertical()
+      .x(d => d[0])
+      .y(d => d[1]);
+
+    return links.map((link, i) => {
       const modifiedLinkCoordinates = {
-        source: [link.source.x + nodeWidth / 2, link.source.y + nodeHeight],
-        target: [link.target.x + nodeWidth / 2, link.target.y]
+        source: [link.source.x + NODE_WIDTH / 2, link.source.y + NODE_HEIGHT],
+        target: [link.target.x + NODE_WIDTH / 2, link.target.y]
       }
       return (
         <path key={i}
               className="link" d={lineLink(modifiedLinkCoordinates)}
-              visibility={hiddenPremises.includes(link.target.data.premise.id) ? "hidden" : "visible"}
+              visibility={this.getHiddenPremises().includes(link.target.data.premise.id) ? "hidden" : "visible"}
         />
       );
     });
 
+  }
+
+  generateTree() {
+    const root = this.generateRoot()
+    let treeLayout = d3.tree();
+    treeLayout.size([TREE_LAYOUT_WIDTH, TREE_LAYOUT_HEIGHT])
+    const tree = treeLayout(root)
+    let nodes = tree.descendants();
+    nodes[0].y += ROOT_NODE_MARGIN
+    const links = tree.links()
+
+    const linkPaths = this.generateLinkPaths(links)
+    const displayNodes = this.generateTreeNodes(nodes)
+
+    return (
+      <g>
+        {linkPaths}
+        {displayNodes}
+      </g>
+    )
+  }
+
+  render() {
+    const {showModal, selectedPremise} = this.state
     return (
       <div>
-        <svg height="800" width="900">
-          {links}
-          {divNodes}
+        <svg height={SVG_LAYOUT_HEIGHT} width={SVG_LAYOUT_WIDTH}>
+          {this.generateTree()}
         </svg>
-        <Modal show={this.state.showModal} onHide={() => this.setState({showModal: false})}>
+        <Modal show={showModal} onHide={() => this.setState({showModal: false})}>
           <Modal.Header closeButton>
             <Modal.Title>Heading</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <EditNewPremise premise={this.state.selectedPremise}/>
+            <EditNewPremise premise={selectedPremise}/>
           </Modal.Body>
         </Modal>
       </div>
